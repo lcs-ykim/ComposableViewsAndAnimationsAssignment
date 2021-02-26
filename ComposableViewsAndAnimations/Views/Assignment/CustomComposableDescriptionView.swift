20694530e4161a69084d000af3a22d3b03d0afcf
Yeseo Kim <YKim@lcs.on.ca>
2021-02-26 18:13:40 -0500

    Assignment completed but still few changes/improvements to make

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableDescriptionView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableDescriptionView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct CustomComposableDescriptionView: View {
     
     // MARK: Stored properties
-    @State private var phrase: String = ""
+    @State private var speed: String = "0.5"
+    @State private var color: Double = 0.0
     
     // MARK: Computed properties
     var body: some View {
@@ -27,28 +28,62 @@ struct CustomComposableDescriptionView: View {
                         .padding(.top)
                     
                     Text("""
-                        Replace this with a description of how to use this view.
-
-                        If the view accepts a parameter, provide a control to enter the input below.
+                        Most apps have a boring loading sign with grey rectangles endlessly rotating. Here is a pleasant-looking loading sign that you can freely choose the color and speed for!
                         """)
                     
-                    TextField("Enter an input value", text: $phrase)
+                }
+                
+                Group {
+                    
+                    Text("Customization")
+                        .font(.title2)
+                        .bold()
+                        .padding(.top)
+                    
+                    Section(header: Text("Choose the Speed. 5 is the fastest.")) {
+                        
+                        Picker("Speed", selection: $speed) {
+                            Text("1").tag(1.0)
+                            Text("2").tag(0.7)
+                            Text("3").tag(0.5)
+                            Text("4").tag(0.3)
+                            Text("5").tag(0.1)
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                    }
+                    
+                    Text("Change the color of the bubble using the slider.")
+                    Slider(value: $color, in: 0...360.0, step: 1.0)
+                    
+                    HStack {
+                        
+                        Spacer()
+                        
+                        Circle()
+                            .fill(Color(hue: color / 360.0, saturation: 1.0, brightness: 1.0))
+                            .frame(width: 20, height: 20, alignment: .center)
+                        
+                        Spacer()
+                        
+                    }
                     
                 }
-                .padding(.bottom)
                 
             }
             
-            NavigationLink(destination: CustomComposableView()) {
-                SimpleListItemView(title: "My Composable View",
-                                   caption: "A brief description of my view")
+            NavigationLink(destination: CustomComposableView(duration: Double(speed)!, hue: color)) {
+                SimpleListItemView(title: "Loading sign",
+                                   caption: "The bubbles will be in your choice of color and so is the speed.")
             }
             
         }
+        
         .padding()
-        .navigationTitle("My Composable View")
+        .navigationTitle("Loading sign")
         
     }
+    
 }
 
 struct CustomComposableDescriptionView_Previews: PreviewProvider {
--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -9,67 +9,79 @@ import SwiftUI
 
 struct CustomComposableView: View {
     
-    @State private var offSetY: CGFloat = 0
+    // MARK: Stored properties
+    // Receive inputs from the main page
+    var duration: Double
+    var hue: Double
+            
+    // Controls the size of the circle
+    @State private var scaleFactorLeft: CGFloat = 1.0
+    @State private var scaleFactorMiddle: CGFloat = 1.0
+    @State private var scaleFactorRight: CGFloat = 1.0
         
-    @State private var hue: Color = .red
+    // Controls the opacity of each circle
+    @State private var opacityLeft = 1.0
+    @State private var opacityMiddle = 1.0
+    @State private var opacityRight = 1.0
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        
-        NavigationView {
-                                            
-                VStack {
                     
-                    Spacer()
-                    
-                    HStack {
-    
-                        Spacer()
-                        
-                        Circle()
-                            .frame(width: 50, height: 50)
-                            .foregroundColor(hue)
-                            .offset(x: 0, y: offSetY)
-                            .onTapGesture {
-                                                            
-                                if offSetY > -150 {
-                                        
-                                        withAnimation(Animation.easeInOut(duration: 0.5)) {
-                                            offSetY -= CGFloat.random(in: 20...150)
-                                        }
-                                        
-                                        withAnimation(Animation.easeInOut(duration: 0.5).delay(0.5)) {
-                                            offSetY = 0
-                                        }
-                                        
-                                    } else {
-                                        
-                                        withAnimation(Animation.easeIn(duration: 0.5).delay(0.5)) {
-                                            offSetY = 0
-                                        }
-                                        
-                                    }
-                                                    
-                                withAnimation() {
-                                    hue = Color(hue: Double.random(in: 1...360) / 360.0,
-                                                saturation: 0.8,
-                                                brightness: 0.8)
-                                }
-                                                            
-                            }
-                        
-                        Spacer()
+        VStack {
+                
+                Spacer()
 
-                    }
-                    
+                HStack {
+
+                    Spacer()
+
+                    Circle()
+                        .fill(Color(hue: hue / 360.0, saturation: 1.0, brightness: 1.0, opacity: opacityLeft))
+                        .frame(width: 20, height: 20)
+                        .scaleEffect(scaleFactorLeft)
+
+                    Circle()
+                        .fill(Color(hue: hue / 360.0, saturation: 1.0, brightness: 1.0, opacity: opacityMiddle))
+                        .frame(width: 20, height: 20)
+                        .scaleEffect(scaleFactorMiddle)
+
+                    Circle()
+                        .fill(Color(hue: hue / 360.0, saturation: 1.0, brightness: 1.0, opacity: opacityRight))
+                        .frame(width: 20, height: 20)
+                        .scaleEffect(scaleFactorRight)
+
                     Spacer()
 
                 }
+
+                Spacer()
+                
             }
+            .navigationTitle("Loading Sign")
+        
+        .onReceive(timer) { input in
+            
+            withAnimation(Animation.easeIn(duration: duration).repeatForever()) {
+                opacityLeft = 0.0
+                scaleFactorLeft = 0.5
+            }
+            withAnimation(Animation.easeIn(duration: duration).repeatForever().delay(0.3)) {
+                opacityMiddle = 0.0
+                scaleFactorMiddle = 0.5
+            }
+            withAnimation(Animation.easeIn(duration: duration).repeatForever().delay(0.6)) {
+                opacityRight = 0.0
+                scaleFactorRight = 0.5
+            }
+            
         }
+        
     }
+}
 
 struct CustomComposableView_Previews: PreviewProvider {
     static var previews: some View {
-        CustomComposableView()
+        CustomComposableView(duration: 0.5, hue: 0.0)
     }
 }
