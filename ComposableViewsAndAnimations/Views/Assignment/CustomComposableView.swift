b60848da8430f79d6265ac01ed7d10c5a392cabe
Yeseo Kim <YKim@lcs.on.ca>
2021-02-25 19:19:58 -0500

    Failed to use GeometryReader but managed to center the ball using Spacer()

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -8,58 +8,65 @@
 import SwiftUI
 
 struct CustomComposableView: View {
-        
-    @State private var offSetX: CGFloat = -130.0
     
     @State private var offSetY: CGFloat = 0
-
+        
     @State private var hue: Color = .red
-
+    
     var body: some View {
         
         NavigationView {
-            
-            VStack {
-                
-                Circle()
-                    .frame(width: 50, height: 50)
-                    .foregroundColor(hue)
-                    .offset(x: offSetX, y: offSetY)
-                    .onTapGesture {
-                        
-                        withAnimation() {
-                            
-                            if offSetX < 120.0 {
-                                
-                                offSetX += 50.0
-                            } else {
-                                
-                                offSetX = -130.0
-                            }
-                            
-                        }
-                        
-                        withAnimation(Animation.easeIn(duration: 0.5).delay(0.5)) {
-                            offSetY -= CGFloat.random(in: 20...150)
-                        }
-                        
-                        withAnimation(Animation.easeIn(duration: 0.5).delay(1.0)) {
-                            offSetY = 0
-                        }
                                             
-                        withAnimation() {
-                            hue = Color(hue: Double.random(in: 1...360) / 360.0,
-                                        saturation: 0.8,
-                                        brightness: 0.8)
-                        }
+                VStack {
                     
+                    Spacer()
+                    
+                    HStack {
+    
+                        Spacer()
+                        
+                        Circle()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(hue)
+                            .offset(x: 0, y: offSetY)
+                            .onTapGesture {
+                                                            
+                                if offSetY > -150 {
+                                        
+                                        withAnimation(Animation.easeInOut(duration: 0.5)) {
+                                            offSetY -= CGFloat.random(in: 20...150)
+                                        }
+                                        
+                                        withAnimation(Animation.easeInOut(duration: 0.5).delay(0.5)) {
+                                            offSetY = 0
+                                        }
+                                        
+                                    } else {
+                                        
+                                        withAnimation(Animation.easeIn(duration: 0.5).delay(0.5)) {
+                                            offSetY = 0
+                                        }
+                                        
+                                    }
+                                                    
+                                withAnimation() {
+                                    hue = Color(hue: Double.random(in: 1...360) / 360.0,
+                                                saturation: 0.8,
+                                                brightness: 0.8)
+                                }
+                                                            
+                            }
+                        
+                        Spacer()
+
                     }
-                
+                    
+                    Spacer()
+
+                }
             }
         }
     }
-    
-}
 
 struct CustomComposableView_Previews: PreviewProvider {
     static var previews: some View {
